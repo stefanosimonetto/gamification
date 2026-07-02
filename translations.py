1a491c8bb3f60752a16ba01c8715557a3431afab
stefano <stefanosimonetto98@gmail.com>
2026-07-02 23:04:46 +0800

    serious game

--- translations.py
+++ translations.py
@@ -1,192 +1,192 @@
 # translations_v3.py
  
-translations = {
-"title": {
-"en": "How Responsible is Your Innovation? ",
-"nl": "Wat is de waarde van uw innovatie?"
-},
+# translations = {
+# "title": {
+# "en": "How Responsible is Your Innovation? ",
+# "nl": "Wat is de waarde van uw innovatie?"
+# },
  
-"subheader": {
-"en": "A Generative AI (Alien Intelligence) Game",
-"nl": "Een waardenmapping spel"
-},
+# "subheader": {
+# "en": "A Generative AI (Alien Intelligence) Game",
+# "nl": "Een waardenmapping spel"
+# },
  
-"welcome_message": {
-"en": "User name",
-"nl": "Voer je naam in om te beginnen."
-},
+# "welcome_message": {
+# "en": "User name",
+# "nl": "Voer je naam in om te beginnen."
+# },
  
-"welcome_message2": {
-"en": "Ciao2",
-"nl": "Voer je naam in om te beginnen."
-},
+# "welcome_message2": {
+# "en": "Ciao2",
+# "nl": "Voer je naam in om te beginnen."
+# },
  
-"welcome_message3": {
-"en": "Ciao3",
-"nl": "Voer je naam in om te beginnen."
-},
+# "welcome_message3": {
+# "en": "Ciao3",
+# "nl": "Voer je naam in om te beginnen."
+# },
  
-"welcome_message4": {
-"en": "Ciao4",
-"nl": "Voer je naam in om te beginnen."
-},
+# "welcome_message4": {
+# "en": "Ciao4",
+# "nl": "Voer je naam in om te beginnen."
+# },
  
-"welcome_message5": {
-"en": "Ciao5",
-"nl": "Voer je naam in om te beginnen."
-},
+# "welcome_message5": {
+# "en": "Ciao5",
+# "nl": "Voer je naam in om te beginnen."
+# },
  
-"welcome_message6": {
-"en": "Ciao6",
-"nl": "Voer je naam in om te beginnen."
-},
- 
- 
-"submit_button": {
-"en": "Let's start to play!",
-"nl": "aten we beginnen met spelen!"
-},
- 
-"username_in_use": {
-"en": "Sorry, this username is already in use. Please select a different one.",
-"nl": "Sorry, deze gebruikersnaam is al in gebruik. Kies een andere."
-},
- 
-"greeting_message": {
-"en": "Hello Earthling named {}! We are interstellar merchants from a distant galaxy. 🛸",
-"nl": "Hallo Aardbewoner genaamd {}! Wij zijn interstellaire handelaren uit een verre melkweg. 🛸"
-},
- 
-"bang_o_introduction": {
-"en": "Martian people told us you guys are really good with technology! So we came to check it out ourselves. Do you have what we need to improve our planet?",
-"nl": "De Marsmensen vertelden ons dat jullie heel goed zijn met technologie! Dus we kwamen zelf even kijken. Hebben jullie wat we nodig hebben om onze planeet te verbeteren?"
-},
- 
-"step1_description": {
-"en": "STEP 1/4: Tell us all about your genius innovation 💡",
-"nl": "STAP 1/4: Vertel ons alles over je geniale innovatie 💡"
-},
- 
-"share_innovation1": {
-"en": "Go ahead, spill the beans about your elica1",
-"nl": "Vertel het ons, deel het geheim van je elica1! We luisteren aandachtig. 🎧"
-},
- 
-"share_innovation2": {
-"en": "Go ahead, spill the beans about your elica2",
-"nl": "Vertel het ons, deel het geheim van je elica2! We luisteren aandachtig. 🎧"
-},
- 
-"share_innovation3": {
-"en": "Go ahead, spill the beans about your elica3",
-"nl": "Vertel het ons, deel het geheim van je elica3! We luisteren aandachtig. 🎧"
-},
-"share_innovation4": {
-"en": "Go ahead, spill the beans about your elica4",
-"nl": "Vertel het ons, deel het geheim van je elica4! We luisteren aandachtig. 🎧"
-},
-"share_innovation5": {
-"en": "Go ahead, spill the beans about your elica5",
-"nl": "Vertel het ons, deel het geheim van je elica5! We luisteren aandachtig. 🎧"
-},
-"share_innovation6": {
-"en": "Go ahead, spill the beans about your elica6",
-"nl": "Vertel het ons, deel het geheim van je elica6! We luisteren aandachtig. 🎧"
-},
-"submit_innovation_description": {
-"en": "Submit the description of your innovation!",
-"nl": "Dien de beschrijving van je innovatie in!"
-},
- 
-"waiting_message": {
-"en": "⏳ ... The merchant aliens are thinking ...⌛",
-"nl": "⏳ ... De handelsaliens zijn aan het nadenken ...⌛"
-},
+# "welcome_message6": {
+# "en": "Ciao6",
+# "nl": "Voer je naam in om te beginnen."
+# },
  
  
-"alien_feedback": {
-"en": "👽 The aliens have provided the following feedback:",
-"nl": "👽 De aliens hebben de volgende feedback gegeven:"
-},
+# "submit_button": {
+# "en": "Let's start to play!",
+# "nl": "aten we beginnen met spelen!"
+# },
  
-"step2_benefits": {
-"en": "STEP 2/4: Tell us all the awesome benefits of your innovation! ⚡",
-"nl": "STAP 2/4: Vertel ons alle geweldige voordelen van je innovatie! ⚡"
-},
+# "username_in_use": {
+# "en": "Sorry, this username is already in use. Please select a different one.",
+# "nl": "Sorry, deze gebruikersnaam is al in gebruik. Kies een andere."
+# },
  
-"innovation_benefits": {
-"en": "What benefits does this innovation bring? Think about how it improves existing solutions or tackles unmet needs. Your insights will help us see its true potential!",
-"nl": "Welke voordelen biedt deze innovatie? Denk na over hoe het bestaande oplossingen verbetert of onbenutte behoeften aanpakt. Jouw inzichten helpen ons om de werkelijke waarde te beoordelen!"
-},
+# "greeting_message": {
+# "en": "Hello Earthling named {}! We are interstellar merchants from a distant galaxy. 🛸",
+# "nl": "Hallo Aardbewoner genaamd {}! Wij zijn interstellaire handelaren uit een verre melkweg. 🛸"
+# },
  
-"submit_benefits": {
-"en": "Submit your benefits!",
-"nl": "Dien je voordelen in!"
-},
+# "bang_o_introduction": {
+# "en": "Martian people told us you guys are really good with technology! So we came to check it out ourselves. Do you have what we need to improve our planet?",
+# "nl": "De Marsmensen vertelden ons dat jullie heel goed zijn met technologie! Dus we kwamen zelf even kijken. Hebben jullie wat we nodig hebben om onze planeet te verbeteren?"
+# },
  
-"step3_counter_benefits": {
-"en": "STEP 3/4: We have questions... and we need examples! 🔎",
-"nl": "STAP 3/4: We hebben vragen... en we willen voorbeelden! 🔎"
-},
+# "step1_description": {
+# "en": "STEP 1/4: Tell us all about your genius innovation 💡",
+# "nl": "STAP 1/4: Vertel ons alles over je geniale innovatie 💡"
+# },
  
-"reply_to_feedback": {
-"en": "Tell us how your innovation works in real-world situations. Who will use it? How will it create impact? Give us some examples, we're curious!",
-"nl": "Vertel ons hoe je innovatie werkt in echte situaties. Wie gaat het gebruiken? Hoe zal het impact maken? Geef ons wat voorbeelden, we zijn nieuwsgierig!"
-},
+# "share_innovation1": {
+# "en": "Go ahead, spill the beans about your elica1",
+# "nl": "Vertel het ons, deel het geheim van je elica1! We luisteren aandachtig. 🎧"
+# },
  
-"submit_counter_argument": {
-"en": "Submit your use cases!",
-"nl": "Dien je voorbeelden in!"
-},
+# "share_innovation2": {
+# "en": "Go ahead, spill the beans about your elica2",
+# "nl": "Vertel het ons, deel het geheim van je elica2! We luisteren aandachtig. 🎧"
+# },
  
-"step3_examples": {
-"en": "STEP 4/4: Let's go deeper... 🔭",
-"nl": "STAP 4/4: Laten we dieper ingaan... 🔭"
-},
+# "share_innovation3": {
+# "en": "Go ahead, spill the beans about your elica3",
+# "nl": "Vertel het ons, deel het geheim van je elica3! We luisteren aandachtig. 🎧"
+# },
+# "share_innovation4": {
+# "en": "Go ahead, spill the beans about your elica4",
+# "nl": "Vertel het ons, deel het geheim van je elica4! We luisteren aandachtig. 🎧"
+# },
+# "share_innovation5": {
+# "en": "Go ahead, spill the beans about your elica5",
+# "nl": "Vertel het ons, deel het geheim van je elica5! We luisteren aandachtig. 🎧"
+# },
+# "share_innovation6": {
+# "en": "Go ahead, spill the beans about your elica6",
+# "nl": "Vertel het ons, deel het geheim van je elica6! We luisteren aandachtig. 🎧"
+# },
+# "submit_innovation_description": {
+# "en": "Submit the description of your innovation!",
+# "nl": "Dien de beschrijving van je innovatie in!"
+# },
  
-"request_for_examples": {
-"en": "The conversation is getting interesting! Let's explore some final aspects of your idea. Answer our previous question carefully...",
-"nl": "Het gesprek wordt interessant! Laten we enkele laatste aspecten van je idee onderzoeken. Beantwoord onze vorige vraag zorgvuldig..."
-},
+# "waiting_message": {
+# "en": "⏳ ... The merchant aliens are thinking ...⌛",
+# "nl": "⏳ ... De handelsaliens zijn aan het nadenken ...⌛"
+# },
  
-"submit_examples": {
-"en": "Submit your answer!",
-"nl": "Dien je antwoord in!!"
-},
  
-"thanks_for_playing": {
-"en": "Stellar solution! ✨\n\nWe’re not just buying one, I’ll tell you that. From now on, consider us your intergalactic partners! A new Silk Road has just opened 🚀",
-"nl": "Sterrenoplossing! ✨\n\nWe kopen er niet maar één, dat kan ik je vertellen. Vanaf nu beschouwen we ons als jouw intergalactische partners! Een nieuwe Zijderoute is zojuist geopend 🚀"
-},
+# "alien_feedback": {
+# "en": "👽 The aliens have provided the following feedback:",
+# "nl": "👽 De aliens hebben de volgende feedback gegeven:"
+# },
  
-"alien_evaluation": {
-"en": "Thanks for sharing your intergalactic solution with us! 🙏🏽",
-"nl": "Bedankt dat je je intergalactische oplossing met ons hebt gedeeld! 🙏🏽"
-},
+# "step2_benefits": {
+# "en": "STEP 2/4: Tell us all the awesome benefits of your innovation! ⚡",
+# "nl": "STAP 2/4: Vertel ons alle geweldige voordelen van je innovatie! ⚡"
+# },
  
-"share_email_if_enjoyed": {
-"en": "If you're interested in the project and want to stay updated, please share your email with us. We promise we won’t share it with the Martian people, we swear! ✉️",
-"nl": "Als je geïnteresseerd bent in het project en op de hoogte wilt blijven, deel dan je e-mail met ons. We beloven dat we het niet zullen delen met de Marsbewoners, echt waar! ✉️"
-},
+# "innovation_benefits": {
+# "en": "What benefits does this innovation bring? Think about how it improves existing solutions or tackles unmet needs. Your insights will help us see its true potential!",
+# "nl": "Welke voordelen biedt deze innovatie? Denk na over hoe het bestaande oplossingen verbetert of onbenutte behoeften aanpakt. Jouw inzichten helpen ons om de werkelijke waarde te beoordelen!"
+# },
  
-"submit_email": {
-"en": "Submit your email!",
-"nl": "Dien je e-mail in!"
-},
+# "submit_benefits": {
+# "en": "Submit your benefits!",
+# "nl": "Dien je voordelen in!"
+# },
  
-"thank_you_for_playing": {
-"en": "🚀 Thank you for playing!",
-"nl": "🚀 Bedankt voor het spelen!"
-},
-"tell_us_how_to_improve": {
-"en": "Please, Tell us how to improve this game",
-"nl": "Vertel ons hoe we dit spel kunnen verbeteren"    
-},
-"submit_feedback": {
-    "en": "Submit Feedback",
-    "nl": "Verzend Feedback"
-    }
-}
+# "step3_counter_benefits": {
+# "en": "STEP 3/4: We have questions... and we need examples! 🔎",
+# "nl": "STAP 3/4: We hebben vragen... en we willen voorbeelden! 🔎"
+# },
+ 
+# "reply_to_feedback": {
+# "en": "Tell us how your innovation works in real-world situations. Who will use it? How will it create impact? Give us some examples, we're curious!",
+# "nl": "Vertel ons hoe je innovatie werkt in echte situaties. Wie gaat het gebruiken? Hoe zal het impact maken? Geef ons wat voorbeelden, we zijn nieuwsgierig!"
+# },
+ 
+# "submit_counter_argument": {
+# "en": "Submit your use cases!",
+# "nl": "Dien je voorbeelden in!"
+# },
+ 
+# "step3_examples": {
+# "en": "STEP 4/4: Let's go deeper... 🔭",
+# "nl": "STAP 4/4: Laten we dieper ingaan... 🔭"
+# },
+ 
+# "request_for_examples": {
+# "en": "The conversation is getting interesting! Let's explore some final aspects of your idea. Answer our previous question carefully...",
+# "nl": "Het gesprek wordt interessant! Laten we enkele laatste aspecten van je idee onderzoeken. Beantwoord onze vorige vraag zorgvuldig..."
+# },
+ 
+# "submit_examples": {
+# "en": "Submit your answer!",
+# "nl": "Dien je antwoord in!!"
+# },
+ 
+# "thanks_for_playing": {
+# "en": "Stellar solution! ✨\n\nWe’re not just buying one, I’ll tell you that. From now on, consider us your intergalactic partners! A new Silk Road has just opened 🚀",
+# "nl": "Sterrenoplossing! ✨\n\nWe kopen er niet maar één, dat kan ik je vertellen. Vanaf nu beschouwen we ons als jouw intergalactische partners! Een nieuwe Zijderoute is zojuist geopend 🚀"
+# },
+ 
+# "alien_evaluation": {
+# "en": "Thanks for sharing your intergalactic solution with us! 🙏🏽",
+# "nl": "Bedankt dat je je intergalactische oplossing met ons hebt gedeeld! 🙏🏽"
+# },
+ 
+# "share_email_if_enjoyed": {
+# "en": "If you're interested in the project and want to stay updated, please share your email with us. We promise we won’t share it with the Martian people, we swear! ✉️",
+# "nl": "Als je geïnteresseerd bent in het project en op de hoogte wilt blijven, deel dan je e-mail met ons. We beloven dat we het niet zullen delen met de Marsbewoners, echt waar! ✉️"
+# },
+ 
+# "submit_email": {
+# "en": "Submit your email!",
+# "nl": "Dien je e-mail in!"
+# },
+ 
+# "thank_you_for_playing": {
+# "en": "🚀 Thank you for playing!",
+# "nl": "🚀 Bedankt voor het spelen!"
+# },
+# "tell_us_how_to_improve": {
+# "en": "Please, Tell us how to improve this game",
+# "nl": "Vertel ons hoe we dit spel kunnen verbeteren"    
+# },
+# "submit_feedback": {
+#     "en": "Submit Feedback",
+#     "nl": "Verzend Feedback"
+#     }
+# }
 
 # translations.py
  
@@ -376,3 +376,147 @@ translations = {
 #     "nl": "Verzend Feedback"
 #     }
 # }
+
+#serious game trans
+# translations_v3.py
+ 
+translations = {
+"title": {
+"en": "What's the value of your innovation?",
+"nl": "Wat is de waarde van uw innovatie?"
+},
+ 
+"subheader": {
+"en": "A value mapping game",
+"nl": "Een waardenmapping spel"
+},
+ 
+"welcome_message": {
+"en": "Please enter your name to get started.",
+"nl": "Voer je naam in om te beginnen."
+},
+ 
+"submit_button": {
+"en": "Let's start to play!",
+"nl": "aten we beginnen met spelen!"
+},
+ 
+"username_in_use": {
+"en": "Sorry, this username is already in use. Please select a different one.",
+"nl": "Sorry, deze gebruikersnaam is al in gebruik. Kies een andere."
+},
+ 
+"greeting_message": {
+"en": "Hello Earthling named {}! We are interstellar merchants from a distant galaxy. 🛸",
+"nl": "Hallo Aardbewoner genaamd {}! Wij zijn interstellaire handelaren uit een verre melkweg. 🛸"
+},
+ 
+"bang_o_introduction": {
+"en": "Martian people told us you guys are really good with technology! So we came to check it out ourselves. Do you have what we need to improve our planet?",
+"nl": "De Marsmensen vertelden ons dat jullie heel goed zijn met technologie! Dus we kwamen zelf even kijken. Hebben jullie wat we nodig hebben om onze planeet te verbeteren?"
+},
+ 
+"step1_description": {
+"en": "STEP 1/4: Tell us all about your genius innovation 💡",
+"nl": "STAP 1/4: Vertel ons alles over je geniale innovatie 💡"
+},
+ 
+"share_innovation": {
+"en": "Go ahead, spill the beans about your awesome innovation! We're all ears 🎧",
+"nl": "Vertel het ons, deel het geheim van je geweldige innovatie! We luisteren aandachtig. 🎧"
+},
+ 
+"submit_innovation_description": {
+"en": "Submit the description of your innovation!",
+"nl": "Dien de beschrijving van je innovatie in!"
+},
+ 
+"waiting_message": {
+"en": "⏳ ... The merchant aliens are thinking ...⌛",
+"nl": "⏳ ... De handelsaliens zijn aan het nadenken ...⌛"
+},
+ 
+ 
+"alien_feedback": {
+"en": "👽 The aliens have provided the following feedback:",
+"nl": "👽 De aliens hebben de volgende feedback gegeven:"
+},
+ 
+"step2_benefits": {
+"en": "STEP 2/4: Tell us all the awesome benefits of your innovation! ⚡",
+"nl": "STAP 2/4: Vertel ons alle geweldige voordelen van je innovatie! ⚡"
+},
+ 
+"innovation_benefits": {
+"en": "What benefits does this innovation bring? Think about how it improves existing solutions or tackles unmet needs. Your insights will help us see its true potential!",
+"nl": "Welke voordelen biedt deze innovatie? Denk na over hoe het bestaande oplossingen verbetert of onbenutte behoeften aanpakt. Jouw inzichten helpen ons om de werkelijke waarde te beoordelen!"
+},
+ 
+"submit_benefits": {
+"en": "Submit your benefits!",
+"nl": "Dien je voordelen in!"
+},
+ 
+"step3_counter_benefits": {
+"en": "STEP 3/4: We have questions... and we need examples! 🔎",
+"nl": "STAP 3/4: We hebben vragen... en we willen voorbeelden! 🔎"
+},
+ 
+"reply_to_feedback": {
+"en": "Tell us how your innovation works in real-world situations. Who will use it? How will it create impact? Give us some examples, we're curious!",
+"nl": "Vertel ons hoe je innovatie werkt in echte situaties. Wie gaat het gebruiken? Hoe zal het impact maken? Geef ons wat voorbeelden, we zijn nieuwsgierig!"
+},
+ 
+"submit_counter_argument": {
+"en": "Submit your use cases!",
+"nl": "Dien je voorbeelden in!"
+},
+ 
+"step3_examples": {
+"en": "STEP 4/4: Let's go deeper... 🔭",
+"nl": "STAP 4/4: Laten we dieper ingaan... 🔭"
+},
+ 
+"request_for_examples": {
+"en": "The conversation is getting interesting! Let's explore some final aspects of your idea. Answer our previous question carefully...",
+"nl": "Het gesprek wordt interessant! Laten we enkele laatste aspecten van je idee onderzoeken. Beantwoord onze vorige vraag zorgvuldig..."
+},
+ 
+"submit_examples": {
+"en": "Submit your answer!",
+"nl": "Dien je antwoord in!!"
+},
+ 
+"thanks_for_playing": {
+"en": "Stellar solution! ✨\n\nWe’re not just buying one, I’ll tell you that. From now on, consider us your intergalactic partners! A new Silk Road has just opened 🚀",
+"nl": "Sterrenoplossing! ✨\n\nWe kopen er niet maar één, dat kan ik je vertellen. Vanaf nu beschouwen we ons als jouw intergalactische partners! Een nieuwe Zijderoute is zojuist geopend 🚀"
+},
+ 
+"alien_evaluation": {
+"en": "Thanks for sharing your intergalactic solution with us! 🙏🏽",
+"nl": "Bedankt dat je je intergalactische oplossing met ons hebt gedeeld! 🙏🏽"
+},
+ 
+"share_email_if_enjoyed": {
+"en": "If you're interested in the project and want to stay updated, please share your email with us. We promise we won’t share it with the Martian people, we swear! ✉️",
+"nl": "Als je geïnteresseerd bent in het project en op de hoogte wilt blijven, deel dan je e-mail met ons. We beloven dat we het niet zullen delen met de Marsbewoners, echt waar! ✉️"
+},
+ 
+"submit_email": {
+"en": "Submit your email!",
+"nl": "Dien je e-mail in!"
+},
+ 
+"thank_you_for_playing": {
+"en": "🚀 Thank you for playing!",
+"nl": "🚀 Bedankt voor het spelen!"
+},
+"tell_us_how_to_improve": {
+"en": "Please, Tell us how to improve this game",
+"nl": "Vertel ons hoe we dit spel kunnen verbeteren"    
+},
+"submit_feedback": {
+    "en": "Submit Feedback",
+    "nl": "Verzend Feedback"
+    }
+}
